1cb82d07c65e3e57455b69693110be50c7e10480
Tiago Catulo <tiagomiguelcatulo@hotmail.com>
2023-06-20 11:39:03 +0100

    Update projetoQuiz.cpp
    Adicionei ecrã de boas vindas e mais uns includes

--- projetoQuiz.cpp
+++ projetoQuiz.cpp
@@ -2,10 +2,21 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <conio.h>
-
-
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <map>
 
 using namespace std;
+
+void bemVindo() {
+    cout << "-------------------------------------" << endl;
+    cout << "--------------BEM VINDO--------------" << endl;
+    cout << "-------------------------------------" << endl;
+    cout << "-----Vamos iniciar o seu registo-----" << endl;
+    cout << "-------------------------------------" << endl;
+}
 
 
 
